6b225ee9aad425e4c4fcb00d62326c4b6f6bfc54
kele_Bingtang <2456019588@qq.com>
2025-10-17 02:56:47 +0800

    fix(point-tag): 修复文本渲染逻辑并优化组件结构
    - 调整插槽渲染顺序以正确显示文本
    - 添加文本存在性检查避免空内容渲染
    - 优化组件内部结构提升可读性
    - 保留自定义样式变量支持
    - 确保左右位置插槽正确显示
    - 修复无文本时的默认渲染逻辑

--- src/components/core/point-tag/src/index.vue
+++ src/components/core/point-tag/src/index.vue
@@ -17,14 +17,20 @@ withDefaults(defineProps<PointTagProps>(), {
 </script>
 
 <template>
-  <slot v-if="position === 'right'">
-    {{ text }}
-  </slot>
+  <template v-if="text">
+    <slot v-if="position === 'right'">{{ text }}</slot>
+    <i
+      :class="[ns.b(), ns.is(position), ns.is(type, !color)]"
+      :style="{ '--point-color': color, '--point-size': size, '--point-offset': offset }"
+    />
+    <slot v-if="position === 'left'">{{ text }}</slot>
+  </template>
+
   <i
+    v-else
     :class="[ns.b(), ns.is(position), ns.is(type, !color)]"
     :style="{ '--point-color': color, '--point-size': size, '--point-offset': offset }"
   />
-  <slot v-if="position === 'left'">{{ text }}</slot>
 </template>
 
 <style lang="scss" scoped>
