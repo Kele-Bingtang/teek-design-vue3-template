9ce447229ea36430003772ef85e019e08c0f1bf3
kele_Bingtang <2456019588@qq.com>
2025-12-10 00:58:04 +0800

    feat(utils): 添加字符串简化函数并优化分栏布局样式
    - 新增 simplifyText 函数用于提取中文首字符或英文单词首字母
    - 优化侧边栏菜单激活状态颜色变量引用
    - 更新菜单图标选择器以支持更多命名空间
    - 调整菜单折叠状态下显示名称为简化文本
    - 修复菜单圆润风格下子菜单标题宽度和边距问题
    - 统一 SCSS 注释风格从双斜杠改为多行注释

--- src/layout/layout-columns/index.vue
+++ src/layout/layout-columns/index.vue
@@ -5,10 +5,10 @@ import { storeToRefs } from "pinia";
 import { ElContainer, ElAside, ElHeader, ElScrollbar } from "element-plus";
 import { Fold, Expand } from "@element-plus/icons-vue";
 import { serviceConfig, HOME_URL } from "@/common/config";
+import { simplifyText } from "@/common/utils";
 import { Tooltip } from "@/components";
-import { useCommon, useMenu } from "@/composables";
+import { useCommon, useMenu, useNamespace } from "@/composables";
 import { useSettingStore } from "@/pinia";
-import { useNamespace } from "@/composables";
 import { useMenuAreaMouse, useHeaderAreaMouse } from "../use-area-mouse";
 import PageContent from "../components/page-content/index.vue";
 import Header from "../components/header/index.vue";
@@ -131,7 +131,9 @@ const changeMenuItem = (item: RouterConfig) => {
       :style="{ ...asideStyle, left: '72px' }"
     >
       <div :class="[ns.e('logo'), ns.join('layout-logo')]" class="flx-center">
-        <span v-show="menuItem.length">{{ menu.collapsed ? "K" : serviceConfig.layout.name }}</span>
+        <span v-show="menuItem.length">
+          {{ menu.collapsed ? simplifyText(serviceConfig.layout.name) : serviceConfig.layout.name }}
+        </span>
       </div>
 
       <el-scrollbar v-if="menuItem?.length">
